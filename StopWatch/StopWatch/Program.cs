6f7c24b19ede2e7f7c03efd1aa63a1de3887a4cc
David Luke <dluke345@gmail.com>
2017-10-30 14:43:25 -0500

    Add files via upload

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            var isRunning = true;
+            Console.WriteLine(
+                "Welcome to the StopWatch App\nThe inputs are as follows: Start, Stop, Duration, Exit");
+            try
+            {
+                do
+                {      
+                    var input = Console.ReadLine();
+
+                    switch (input)
+                    {         
+                        case "Start":
+                            Console.WriteLine("\nStarting StopWatch\n");
+                            stopWatch.Start();
+                            break;
+                        case "Stop":
+                            Console.WriteLine("\nStopping StopWatch\n");
+                            stopWatch.Stop();
+                            break;                       
+                        case "Duration":
+                            Console.WriteLine("\nDuration of time was: {0}", stopWatch.Duration());
+                            break;
+                        case "Exit":
+                            Console.WriteLine("\nExiting the StopWatch\n");
+                            isRunning = false;
+                            break;
+                    } 
+                } while (isRunning);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+
+        }
+    }
+}
---
+++ StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime StartTime { get; set; }
+        private DateTime StopTime { get; set; }
+        private bool IsStarted { get; set; }
+
+        public StopWatch()
+        {
+            StartTime = DateTime.MinValue;
+            StopTime = DateTime.MinValue;
+            IsStarted = false;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                if (IsStarted)
+                {
+                    throw new InvalidOperationException("StopWatch is already running.");
+                }
+                IsStarted = true;
+                StartTime = DateTime.Now;
+                StopTime = DateTime.MinValue;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                if (!IsStarted)
+                {
+                    throw new InvalidOperationException("StopWatch is not running.");
+                }
+                IsStarted = false;
+                StopTime = DateTime.Now;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        //Don't need a setter because we will never be setting the duration.
+        public TimeSpan Duration()
+        {
+            if (IsStarted)
+            {
+                return new TimeSpan();
+            }
+            return StopTime - StartTime;
+        }
+    }
+}
