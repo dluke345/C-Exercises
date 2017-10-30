75f18609d0f2db2a4c585ac3b4cf793ac6f6f27b
David Luke <dluke345@gmail.com>
2017-10-30 14:42:49 -0500

    Add files via upload

---
+++ StackOverflowPost/StackOverflowPost/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; private set; }
+        public int VoteCount { get; private set; }
+
+        public Post(string title, string desc)
+        {
+            Title = title;
+            Description = desc;
+            CreatedAt = DateTime.Now;
+        }
+
+
+        public void UpVote()
+        {
+            VoteCount++;
+        }
+
+        public void DownVote()
+        {
+            VoteCount--;    
+        }
+    }
+}
---
+++ StackOverflowPost/StackOverflowPost/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var isRunning = true;
+            Console.WriteLine("Welcome to StackOverflowPost, enter a title.");
+            var input1 = Console.ReadLine();
+            Console.WriteLine("Enter a description.");
+            var input2 = Console.ReadLine();
+
+            var post1 = new Post(input1, input2);
+
+            Console.WriteLine("Commands: ViewPost, UpVote, DownVote, VoteCount, Exit");
+            
+
+            do
+            {
+                var input3 = Console.ReadLine();
+                switch (input3)
+                {
+                    case "ViewPost":
+                        Console.WriteLine("Title: {0}\nDescription: {1}\nCreated: {2}\nVoteCount: {3}", post1.Title, post1.Description, post1.CreatedAt, post1.VoteCount);
+                        break;
+                    case "UpVote":
+                        post1.UpVote();
+                        Console.WriteLine("+1");
+                        break;
+                    case "DownVote":
+                        post1.DownVote();
+                        Console.WriteLine("-1");
+                        break;
+                    case "VoteCount":
+                        Console.WriteLine("Current Vote Count: " + post1.VoteCount);
+                        break;
+                    case "Exit":
+                        isRunning = false;
+                        break;
+                }
+            } while (isRunning);
+
+        }
+    }
+}
