e411b37a996e4dd8d3205d20196a813ca6c6b8e3
David Luke <dluke345@gmail.com>
2017-10-30 14:41:07 -0500

    Add files via upload

---
+++ DesignAStack/DesignAStack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignAStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //A stack is a data structure for storing a list of elements in a LIFO(last in, first out) fashion.
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+
+
+        }
+    }
+}
---
+++ DesignAStack/DesignAStack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DesignAStack
+{
+    public class Stack
+    {
+        //Could use either of these fields
+        //private readonly ArrayList _list = new ArrayList();
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            //Don't store null references
+            try
+            {
+                if (obj == null)
+                {
+                    throw new InvalidOperationException("obj should not be null");                
+                }
+                _list.Add(obj);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public object Pop()
+        {
+            //Need to check for an empty stack and throw exception if there is
+            if (_list.Count <= 0)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+            var result = _list[_list.Count - 1];
+            _list.RemoveAt(_list.Count - 1);
+            return result;
+        }
+
+        public void Clear()
+        {
+            //Needs to remove all objects from the stack
+            _list.Clear();
+        }
+    }
+}
